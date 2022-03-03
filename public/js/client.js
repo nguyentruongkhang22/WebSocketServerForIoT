d63aff43c587a248c5d3ba368082fde95f3fd7f9
Khang Nguyen <nguyentruongkhang22@gmail.com>
2022-03-04 00:14:31 +0700

    minor changes

--- public/js/client.js
+++ public/js/client.js
@@ -4,7 +4,12 @@ socket.on('on', () => {
 });
 
 const change = async (thisElement) => {
+    const currentTime = await new Date().toUTCString();
     socket.emit('change', thisElement.checked);
+    const historyActions = await document.getElementById('history');
+    historyActions.innerHTML =
+        `<p>Device turn: <strong>${thisElement.checked ? 'ON' : 'OFF'}</strong> at ${currentTime}</p>` +
+        historyActions.innerHTML;
 };
 
 socket.on('testing', (e) => {
--- routes/routes.js
+++ routes/routes.js
@@ -3,18 +3,20 @@ const http = require('http');
 
 const Device = require('../model/deviceModel');
 const controller = require('../controllers/controller');
-const { get } = require('express/lib/response');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index.ejs');
-});
-router.get('/add-new-device', (req, res) => {
-    res.render('addNewDevice.ejs');
-});
+// router.get('/', (req, res) => {
+//     res.render('index.ejs');
+// });
+// router.get('/add-new-device', (req, res) => {
+//     res.render('addNewDevice.ejs');
+// });
 
-// router.get('/devices-list', controller.getDevicesList);
+// router.get('/devices-list', (req, res) => {
+//     console.log('haha');
+//     res.sendFile(`./allDevice.html`);
+// });
 // router.get('/device/:id', controller.controlDevice);
 // router.route('/api/v1/devices').get(controller.getAllDevices).post(controller.addNewDevice);
 // router.route('/api/v1/device/:id').delete(controller.removeDevice).get(controller.getDevice);
